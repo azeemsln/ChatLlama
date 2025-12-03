084ef82475e6cb865461d7854f85a194caf07970
azeemsln <mdazeem2498@gmail.com>
2025-12-03 10:46:54 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "background": '#37353E',
+      },
+    },
+  },
+  plugins: [],
+}
